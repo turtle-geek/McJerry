e05190d431ab882f0b1167515a5e112ca36fd076
jackyleunghh <jackyleunghh@gmail.com>
2025-11-25 10:24:36 -0500

    feat: added xml file for inventory

--- app/src/main/java/com/example/myapplication/health/MedicineLabel.java
+++ app/src/main/java/com/example/myapplication/health/MedicineLabel.java
@@ -1,12 +1,6 @@
 package com.example.myapplication.health;
 
-public enum HealthInfo {
-    RESCUE_LOGS,
-    CONTROLLER_ADHERENCE,
-    SYMPTOMS,
-    TRIGGERS,
-    PEF_LOG,
-    PEF_PB,
-    TRIAGE_INCIDENTS,
-    CHARTS;
+public enum MedicineLabel {
+    CONTROLLER,
+    RESCUE;
 }
